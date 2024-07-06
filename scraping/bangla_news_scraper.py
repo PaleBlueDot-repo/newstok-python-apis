dcaa14eb847d41c959fa01461b1162aa2c480f23
ynnuSunny <sunnysutradhar38@gmail.com>
2024-07-06 12:38:22 +0600

    add bangla draft scrapper

--- scraping/bangla_news_scraper.py
+++ scraping/bangla_news_scraper.py
@@ -9,42 +9,67 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 newspaper_base_url = 'https://www.bangla.24livenewspaper.com/'
+max_pages_index = 10
+
+def contains_url(text):
+    url = "https://www.bangla.24livenewspaper.com/post-"
+    return url in text
 
 def scrape_bangla_news(topic):
     output_result = []
-    index = 0  # Initialize index here
-
-    # Setup Selenium with ChromeDriverManager
+    index = 1
+     # Setup Selenium with ChromeDriverManager
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Run headless Chrome
+    # chrome_options.add_argument('--headless')  # Run headless Chrome
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    while index < 10:
-        index += 10
-        url = newspaper_base_url + topic + '?start=' + str(index)
-        print("Fetching URL:", url)
+    while index<=max_pages_index:
+        url = 'https://www.bangla.24livenewspaper.com/bangladesh?start=13'
+        print("Fetching URL:", "https://www.bangla.24livenewspaper.com/bangladesh?start=13")
         
         driver.get(url)
-        try:
-            WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((By.XPATH, '//h2[@class="item-title"]/a'))
-            )
-        except:
-            print("Timeout waiting for element")
-            break  # Exit the loop or handle as needed
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//h2/a'))
+        )
 
         # Find all <h2> tags with class 'item-title'
-        
-        h2_tags = driver.find_elements(By.XPATH, '//h2[@class="item-title"]/a')
+        h2_tags = driver.find_elements(By.XPATH, '//h2/a')
         print(f"Found {len(h2_tags)} <h2> tags with class 'item-title'")
+        news_info = []
+        for h2_tag in h2_tags:
+            title = h2_tag.text
+            link = h2_tag.get_attribute('href')
+            if(contains_url(link)):
+                news_info.append({'title': title, 'link': link})
+        for news in news_info:
+            url = news['link']
+            if(url):
+                driver.get(url)
+                try:
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH, "//div[@class='item-page']/div/p"))
+                    )
+                except:
+                    continue
 
-        if not h2_tags:
-            break
+                # Find the elements
+                article_elements = driver.find_elements(By.XPATH, "//div[@class='item-page']/div/p")
 
-       
+                # Extract the text from each element and combine into a single string
+                article_texts = [element.text for element in article_elements]
+                article = " ".join(article_texts)
+
+                time_elements = driver.find_element(By.XPATH, "//*[@class='published']/time")
+
+                # Extract the datetime attribute from each element
+                datetimes = time_elements.get_attribute('datetime')
+                output_result.append({'title': news['title'], 'link': news['link'], 'article': article, 'published': datetimes})
+
+        
+        index += 10  # Increment index to fetch the next set of articles
 
     driver.quit()
     return output_result
--- srp.py
+++ srp.py
@@ -16,36 +16,33 @@ def scrape_bangla_news(topic):
 
     # Setup Selenium with ChromeDriverManager
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Run headless Chrome
+    # chrome_options.add_argument('--headless')  # Run headless Chrome
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    while index < 10:
-        url = newspaper_base_url + topic + '?start=' + str(index)
-        print("Fetching URL:", url)
-        
-        driver.get(url)
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//h2[@class="item-title"]/a'))
-        )
+    
+    url = 'https://www.bangla.24livenewspaper.com/bangladesh?start=13'
+    print("Fetching URL:", "https://www.bangla.24livenewspaper.com/bangladesh?start=13")
+    
+    driver.get(url)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//h2/a'))
+    )
 
-        # Find all <h2> tags with class 'item-title'
-        h2_tags = driver.find_elements(By.XPATH, '//h2[@class="item-title"]/a')
-        print(f"Found {len(h2_tags)} <h2> tags with class 'item-title'")
+    # Find all <h2> tags with class 'item-title'
+    h2_tags = driver.find_elements(By.XPATH, '//h2/a')
+    print(f"Found {len(h2_tags)} <h2> tags with class 'item-title'")
 
-        if not h2_tags:
-            break
+    for h2_tag in h2_tags:
+        title = h2_tag.text
+        link = h2_tag.get_attribute('href')
+        output_result.append({'title': title, 'link': link})
+    
+    index += 10  # Increment index to fetch the next set of articles
 
-        for h2_tag in h2_tags:
-            title = h2_tag.text
-            link = h2_tag.get_attribute('href')
-            output_result.append({'title': title, 'link': link})
-        
-        index += 10  # Increment index to fetch the next set of articles
-
-    driver.quit()
+    # driver.quit()
     return output_result
 
 # Example usage

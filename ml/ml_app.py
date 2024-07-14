878ba7f4ce58f60ca4d81df9d51b974f622089c5
ynnuSunny <sunnysutradhar38@gmail.com>
2024-07-14 23:30:54 +0600

    intigrate fakenuser detector under ml app

--- ml/ml_app.py
+++ ml/ml_app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Blueprint, request, jsonify
 from ml.summarization_gemei_api import process_text
+from ml.fake_news_detect_api import indentify_fake_news
 
 
 ml_bp = Blueprint('ml_bp', __name__)
@@ -14,3 +15,14 @@ def process():
 
     result = process_text(input_text)
     return jsonify(json.loads(result))
+
+@ml_bp.route('/classify', methods=['POST'])
+def classify_text():
+    data = request.get_json()
+    text = data['text']
+    print(text)
+    
+    result = indentify_fake_news(text)
+    
+    # Return the result as JSON
+    return jsonify(result)

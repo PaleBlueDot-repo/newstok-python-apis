1121ba72237d440ea5764d66137eafbb57688214
foyez-hub <emamhasan9203@gmail.com>
2024-08-23 00:40:45 +0600

    secuire api endpoint with a api key

--- ml/generate_image_api.py
+++ ml/generate_image_api.py
@@ -1,9 +1,14 @@
-
+import os
+from dotenv import load_dotenv
 import requests
 
+# Load environment variables from .env file
+load_dotenv()
 
+# Retrieve API key from environment variable
+API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 API_URL = "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": f"Bearer {API_KEY}"}
 
 def query_huggingface_api(prompt):
     try:
@@ -13,6 +18,3 @@ def query_huggingface_api(prompt):
     except requests.exceptions.RequestException as e:
         print(f"Error querying API: {e}")
         return None
-
-
-
--- scraping/scraping_app.py
+++ scraping/scraping_app.py
@@ -1,10 +1,31 @@
 from flask import Blueprint, request, jsonify
 from scraping.bangla_news_scraper import scrape_bangla_news
 from scraping.english_news_scraper import scrape_english_news
+from dotenv import load_dotenv
+import os
+from functools import wraps
+
 
 scraping_bp = Blueprint('scraping_bp', __name__)
+load_dotenv()
+api_key_auth = os.getenv("AUTH_API_KEY")
+
+
+# Sample list of valid API keys for simplicity
+# In production, consider storing these in a secure environment or database
+VALID_API_KEYS = {api_key_auth}
+
+def require_api_key(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        api_key = request.headers.get('x-api-key')
+        if not api_key or api_key not in VALID_API_KEYS:
+            return jsonify({"error": "Invalid or missing API key"}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 @scraping_bp.route('/scrape_bangla_news', methods=['GET'])
+@require_api_key
 def scrape_bangla():
     topic = request.args.get('topic')
     if not topic:
@@ -14,6 +35,7 @@ def scrape_bangla():
     return jsonify({'results': results})
 
 @scraping_bp.route('/scrape_english_news', methods=['GET'])
+@require_api_key
 def scrape_english():
     topic = request.args.get('topic')
     if not topic:
